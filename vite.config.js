135b28b02df82418fd6024bfeda86f301ee5917b
Anton Andersson <antonanderssonphoto@gmail.com>
2025-10-15 13:31:12 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vite';
+
+const isVercel = !!process.env.VERCEL     // Vercel sets this env var
+const repoBase = '/your-repo-name/'       // used only for GitHub Pages
+
 export default defineConfig({
   base: '/ThreeJS-showcase/',  // exact repo name
 });
